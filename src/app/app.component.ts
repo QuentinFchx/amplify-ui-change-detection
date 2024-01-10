5a28723cd475e499a158cac1f6f702241235fa3d
Quentin Focheux <quentin@privateaser.com>
2024-01-10 15:32:17 +0100

    reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'amplify';
+  unsubscribe: (() => void) | null = null;
+
+  constructor(
+    private authenticator: AuthenticatorService,
+    private cdRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    // Forcing a change detection "fixes" the issue
+    // this.unsubscribe = this.authenticator.subscribe(() => {
+    //   this.cdRef.detectChanges();
+    // }).unsubscribe;
+  }
+
+  ngOnDestroy() {
+    if (this.unsubscribe) this.unsubscribe();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { Amplify } from 'aws-amplify';
 import { AppModule } from './app/app.module';
 
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      //  Amazon Cognito User Pool ID
+      userPoolId: 'redacted',
+      // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+      userPoolClientId: 'redacted',
+      // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+      identityPoolId: 'redacted',
+      // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+      // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+      signUpVerificationMethod: 'code', // 'code' | 'link'
+    },
+  },
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
